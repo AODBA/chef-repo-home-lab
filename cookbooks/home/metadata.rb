88b6c8836689663fa7ee539782e1e47dff1123e7
AODBA <opreaadrian1@gmail.com>
2015-06-19 20:28:18 -0300

    Update metadata.rb

--- cookbooks/home/metadata.rb
+++ cookbooks/home/metadata.rb
@@ -1,6 +1,6 @@
 name             'home'
 maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email 'a@a'
 license          'All rights reserved'
 description      'Installs/Configures home'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
